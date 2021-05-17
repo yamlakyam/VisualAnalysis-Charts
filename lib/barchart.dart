b712ac9c208cc84c4dac6e8e1df930e8b19a009c
yam <yamlakyam@gmail.com>
2021-05-17 15:21:27 +0300

    added a new route using a timer

---
+++ lib/barchart.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart';
+
+class barchartt extends StatefulWidget {
+  const barchartt({Key key}) : super(key: key);
+
+  @override
+  _barcharttState createState() => _barcharttState();
+}
+
+class _barcharttState extends State<barchartt> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
+import 'dart:async';
+import 'package:charts_in_flutterr/barchart.dart' as bC;
 
 void main() {
   runApp(MyApp());
@@ -15,10 +17,20 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   List<charts.Series<PieData, String>> _pieData;
 
+  route() {
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (context) =>bC.barchartt()
+        )
+    );
+  }
+
   @override
   void initState() {
     super.initState();
     _pieData = List<charts.Series<PieData, String>>.empty(growable: true);
+    startTime();
   }
 
   generateData() {
@@ -29,6 +41,7 @@ class _MyAppState extends State<MyApp> {
       new PieData('Sleep', 19.2),
       new PieData('Other', 10.3)
     ];
+
 
     _pieData.add(
       charts.Series(
@@ -50,16 +63,20 @@ class _MyAppState extends State<MyApp> {
             generateData(),
             animate: true,
             animationDuration: Duration(seconds: 5),
-            defaultRenderer: charts.ArcRendererConfig(
-                arcWidth: 100,
-                arcRendererDecorators: [
-                  charts.ArcLabelDecorator(
-                      labelPosition: charts.ArcLabelPosition.inside)
-                ]),
+            defaultRenderer:
+                charts.ArcRendererConfig(arcWidth: 100, arcRendererDecorators: [
+              charts.ArcLabelDecorator(
+                  labelPosition: charts.ArcLabelPosition.inside),
+            ]),
           ),
         ),
       ),
     );
+  }
+
+  startTime() async {
+    var duration = new Duration(seconds: 6);
+    return Timer(duration, route);
   }
 }
 
